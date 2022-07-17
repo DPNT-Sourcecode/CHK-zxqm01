00e42afff7efcfa90a323fc026aa325b9cd6db64
sbx_user1051 <sbx_user1051@169.254.13.165>
2022-07-17 21:15:12 +0000

    Sun Jul 17 21:15:12 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,12 +1,10 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
-
-def sum(num1, num2):
-    if not isinstance(num1, int):
+    if not isinstance(x, int):
         return False
-    if not isinstance(num2, int):
+    if not isinstance(y, int):
         return False
-    total = num1 + num2
+    total = x + y
     return total
+
 
