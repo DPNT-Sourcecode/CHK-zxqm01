d3c7530b31bc05c1bfc5f1c23bdabb9254f0a6da
sbx_user1051 <sbx_user1051@169.254.13.165>
2022-07-17 21:29:38 +0000

    Sun Jul 17 21:29:38 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,17 @@
-
+"""
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+"""
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    if skus not in ["A","B","C","D"]:
+        return -1
+
